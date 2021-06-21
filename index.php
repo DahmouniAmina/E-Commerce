1a906ddeaacfa98697cc5d35c3f17ded73aa8cf6
dahmouni <a.dahmouni_etu@enset-media.ac.ma>
2021-06-21 18:37:44 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,172 @@
+<?php
+require "config/constants.php";
+session_start();
+if(isset($_SESSION["uid"])){
+	header("location:profile.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>AminaDH_Site</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<style></style>
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top" style='background-color: #606060'>
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only">navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="index.php" class="navbar-brand" style="color:#FEFEFF">Ecommerce DH_Minee</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php" style="color:#FEFEFF"><span class="glyphicon glyphicon-home" style="color:#FEFEFF"></span> Home</a></li>
+				<li><a href="index.php" style="color:#FEFEFF"><span class="glyphicon glyphicon-modal-window" style="color:#FEFEFF"></span> Products</a></li>
+			</ul>
+			<form class="navbar-form navbar-left">
+		        <div class="form-group">
+		          <input type="text" class="form-control" placeholder="Search" id="search">
+		        </div>
+		        <button type="submit" class="btn btn-primary" id="search_btn" style="background-color:#DF3D2E"><span class="glyphicon glyphicon-search"></span></button>
+		     </form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color:#FEFEFF"><span class="glyphicon glyphicon-shopping-cart" style="color:#FEFEFF"></span> Panier <span class="badge" >0</span></a>
+					<div class="dropdown-menu" style="width:400px;">
+						<div class="panel panel-success">
+							<div class="panel-heading">
+								<div class="row">
+									<div class="col-md-3">Sl.No</div>
+									<div class="col-md-3">Product Image</div>
+									<div class="col-md-3">Product Name</div>
+									<div class="col-md-3">Price in <?php echo CURRENCY; ?></div>
+								</div>
+							</div>
+							<div class="panel-body">
+								<div id="cart_product">
+								</div>
+							</div>
+							<div class="panel-footer"></div>
+						</div>
+					</div>
+				</li>
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"style="color:#FEFEFF"><span class="glyphicon glyphicon-user" style="color:#FEFEFF"></span> Login/Register</a>
+					<ul class="dropdown-menu">
+						<div style="width:300px;">
+							<div class="panel panel-primary" style="border-color:#FEFEFF">
+								<div class="panel-heading" style="background-color:#DF3D2E;border-color:#FEFEFF">Login</div>
+								<div class="panel-heading" style='background-color: #606060'>
+									<form onsubmit="return false" id="login">
+										<label for="email">Email</label>
+										<input type="email" class="form-control" name="email" id="email" required/>
+										<label for="email">Password</label>
+										<input type="password" class="form-control" name="password" id="password" required/>
+										<p><br/></p>
+										<input type="submit" class="btn btn-warning" style="background-color:#DF3D2E " value="Login">
+										<a href="customer_registration.php?register=1" style="color:white; text-decoration:none;">Create Account Now</a>
+									</form>
+								</div>
+								<div class="panel-footer" id="e_msg"></div>
+							</div>
+						</div>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>	
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-1"></div>
+			
+			<div class="col-md-8 col-xs-12">
+				<div class="row">
+					<div class="col-md-12 col-xs-12" id="product_msg">
+					</div>
+				</div>
+				<div class="panel panel-info">
+					<div class="panel-heading" style='background-color: #606060 '>Products</div>
+					<div class="panel-body">
+						<div id="get_product">
+							<!--Here we get product jquery Ajax Request-->
+						</div>
+					</div>
+					
+					<div class="panel-footer">&copy; <?php echo date("Y"); ?></div>
+				</div>
+			</div>
+			<div class="col-md-2 col-xs-12">
+				<div id="get_category">
+				</div>
+				<div id="get_brand">
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ user.php
@@ -0,0 +1,128 @@
+<?php
+//require "config/constants.php";
+
+session_start();
+if(!isset($_SESSION["uid"])){
+	header("location:index.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ecommerce</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<style>
+			table tr td {padding:10px;}
+		</style>
+	</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand">Ecommerce</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+				<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
+			</ul>
+		</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+	
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="panel panel-default">
+					<div class="panel-heading"></div>
+					<div class="panel-body">
+						<h1>Commandes details</h1>
+						<hr/>
+						<?php
+							include_once("Connexion_db.php");
+							$user_id = $_SESSION["uid"];
+							$orders_list = "SELECT o.IDCOMMANDE,o.IDUSER,li.SKU,li.QUANTITE,p.NAME,p.PRICE,p.IMAGE FROM commande o,lignecommande li ,produit p WHERE o.IDUSER='$user_id' AND O.IDCOMMANDE = li.IDCOMMANDE AND li.SKU=p.SKU";
+							$query = mysqli_query($conn,$orders_list);
+							if (mysqli_num_rows($query) > 0) {
+								while ($row=mysqli_fetch_array($query)) {
+									?>
+										<div class="row">
+											<div class="col-md-6">
+												<img style="float:right;" src="product_images/<?php echo $row['IMAGE']; ?>" class="img-responsive img-thumbnail"/>
+											</div>
+											<div class="col-md-6">
+												<table>
+													<tr><td>Product Name</td><td><b><?php echo $row["NAME"]; ?></b> </td></tr>
+													<tr><td>Product Price</td><td><b><?php echo  CURRENCY." ".$row["PRICE"]; ?></b></td></tr>
+													<tr><td>Quantity</td><td><b><?php echo $row["QUANTITE"]; ?></b></td></tr>
+												</table>
+											</div>
+										</div>
+									<?php
+								}
+							}
+						?>
+						
+					</div>
+					<div class="panel-footer"></div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
